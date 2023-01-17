2b9f1b7f390d8d726528ea8abceb430ea599a619
Bartosz Laskowski <laskowski.bartosz99@gmail.com>
2023-01-17 02:12:08 +0100

    json testing

--- trainscraper/jsontest.py
+++ trainscraper/jsontest.py
@@ -3,4 +3,8 @@ import json
 with open(r'C:\Users\lasko\Desktop\scrapy\trainscraper\trains.json') as f:
    data = json.load(f)
 
-print(data[0]["Number"].split()[1])
+for i in range (0,len(data)):
+   if len(data[i]["Number"].split()) > 1:
+      print(data[i]["Number"].split()[1])
+   else:
+      print(data[i]["Number"])
